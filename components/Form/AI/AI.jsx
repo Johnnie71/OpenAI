9c760f2a58c0150f6003b98f5c375b135b1fc2f2
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 18:39:13 -0400

    removed form state

--- components/Form/AI/AI.jsx
+++ components/Form/AI/AI.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Form from "../Form";
 
 const AI = () => {
-	const [directions, setDirections] = useState("");
 	const [responses, setResponses] = useState([]);
 	console.log(responses);
 	return (
