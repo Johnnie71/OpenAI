935987202d38d3be109ffff31f6c5138844c1fca
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-19 22:58:49 -0400

    removed useState hooks

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
 
 export default function Home() {
-	const [directions, setDirections] = useState("");
-	const [list, setList] = useState([]);
 	return (
 		<div className={styles.container}>
 			<Head>
