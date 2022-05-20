1213ab0ad2888a1dc31156e82adf0891bf23dc06
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 17:50:56 -0400

    log testing for API call

--- components/Form/AI/AI.jsx
+++ components/Form/AI/AI.jsx
@@ -4,7 +4,7 @@ import Form from "../Form";
 const AI = () => {
 	const [directions, setDirections] = useState("");
 	const [responses, setResponses] = useState([]);
-
+	console.log(responses);
 	return (
 		<div>
 			<Form directions={directions} setDirections={setDirections} />
