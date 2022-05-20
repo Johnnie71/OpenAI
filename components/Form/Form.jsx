f7eef6b3914dc02b52be60d4da2b2b155e079a1b
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-19 23:11:51 -0400

    adding submit handler function

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 
 const Form = () => {
 	const [directions, setDirections] = useState("");
+
+	async function submitHandler(e) {
+		e.preventDefault();
+	}
+
 	return (
-		<form onSubmit={null}>
+		<form onSubmit={submitHandler}>
 			<input
 				type="text-area"
 				name="input"
 				placeholder="Create a sentence that consists of directions"
+				value={directions}
+				onChange={(e) => setDirections(e.target.value)}
 			/>
 			<input type="submit" value="Generate directions" />
 		</form>
