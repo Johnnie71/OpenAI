9d18fdb023679b114c047c4acf6d860d2d78df22
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 20:49:01 -0400

    displaying list

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const List = ({ results }) => {
-	return <div>List</div>;
+	return (
+		<div>
+			{results.map((res, idx) => (
+				<>
+					<h1 key={idx}>Prompt: {res.prompt}</h1>
+					<p>{res.result}</p>
+				</>
+			))}
+		</div>
+	);
 };
 
 export default List;
