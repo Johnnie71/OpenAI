2a91ef2c92cd8f0948f80d82b82f437bfcea37c7
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-21 01:23:14 -0400

    added grid layout for list

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import { Box, Grid, GridItem, Flex } from "@chakra-ui/react";
 
 const List = ({ results }) => {
+	const tests = [
+		{
+			prompt:
+				"This is what you came for.............. ......... ....... ......... ........ ......",
+			response: "Rihanna",
+		},
+		{ prompt: "Imma hit you with a dose!", response: "Ciara" },
+		{ prompt: "Turn up the music!", response: "Chris Brown" },
+	];
 	return (
-		<div>
-			{results.map((res, idx) => (
-				<>
-					<h1 key={idx}>Prompt: {res.prompt}</h1>
-					<p>Response: {res.result}</p>
-				</>
-			))}
-		</div>
+		<Flex maxW="full" align="center" justify="center">
+			<Grid w="50%" autoColumns gap={5}>
+				{tests.map((result, idx) => (
+					<Box borderRadius="10px" width="100%" bg="teal.200" key={idx}>
+						<Flex>
+							<GridItem p={5}>Prompt:</GridItem>
+							<GridItem ml={20} p={5}>
+								{result.prompt}
+							</GridItem>
+						</Flex>
+						<Flex>
+							<GridItem p={5}>Response:</GridItem>
+							<GridItem ml={16} p={5}>
+								{result.response}
+							</GridItem>
+						</Flex>
+					</Box>
+				))}
+			</Grid>
+		</Flex>
 	);
 };
 
