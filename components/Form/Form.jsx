d50c7b2e519a604d23e53e3c53268d28e663042f
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 23:23:59 -0400

    setIsloading and eddor states

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
-import { FormControl, Button, Textarea, Flex, Box } from "@chakra-ui/react";
+import {
+	FormControl,
+	Button,
+	Textarea,
+	Flex,
+	Box,
+	FormErrorMessage,
+	CircularProgress,
+} from "@chakra-ui/react";
 
 const Form = ({ setResults }) => {
 	const [directionsInput, setDirectionsInput] = useState("");
-	const [loading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
 
 	async function submitHandler(e) {
 		e.preventDefault();
-
-		const response = await fetch("api/generate", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				prompt: directionsInput,
-				directions: directionsInput,
-			}),
-		});
-		const data = await response.json();
-		// console.log(data.prompt, data.result);
-		setResults(data);
-		setDirectionsInput("");
+		setIsLoading(true);
+		try {
+			const response = await fetch("api/genera", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					prompt: directionsInput,
+					directions: directionsInput,
+				}),
+			});
+			const data = await response.json();
+			setResults(data);
+			setDirectionsInput("");
+			setIsLoading(false);
+		} catch {
+			setError("Something went wrong! Try again.");
+			setIsLoading(false);
+			setDirectionsInput("");
+		}
 	}
 
 	const changeHandler = (e) => {
@@ -34,6 +49,7 @@ const Form = ({ setResults }) => {
 				<Box my={4}>
 					<form onSubmit={submitHandler}>
 						<FormControl isRequired>
+							{error && <FormErrorMessage>{error}</FormErrorMessage>}
 							<Textarea
 								placeholder="Create a sentence that consists of directions"
 								value={directionsInput}
@@ -42,7 +58,11 @@ const Form = ({ setResults }) => {
 							/>
 						</FormControl>
 						<Button width="full" mt={4} type="submit">
-							Submit
+							{isLoading ? (
+								<CircularProgress isIndeterminate size="24px" color="teal" />
+							) : (
+								"Submit"
+							)}
 						</Button>
 					</form>
 				</Box>
