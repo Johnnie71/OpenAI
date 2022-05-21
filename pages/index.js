7b49dc18f43caf7068ed3b919e38e6f095430378
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 21:52:37 -0400

    changed title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Chat </title>
+				<title>Fun with AI</title>
 				<meta name="description" content="OpenAI" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
