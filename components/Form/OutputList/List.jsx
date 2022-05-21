4267b03c240b1c5677f9f3f2bda1d99e14c3ed3d
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 23:35:25 -0400

    added response header

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -6,7 +6,7 @@ const List = ({ results }) => {
 			{results.map((res, idx) => (
 				<>
 					<h1 key={idx}>Prompt: {res.prompt}</h1>
-					<p>{res.result}</p>
+					<p>Response: {res.result}</p>
 				</>
 			))}
 		</div>
