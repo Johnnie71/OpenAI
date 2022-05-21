f39e05f91a6d9674dbec03895221557d07143243
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-20 20:35:50 -0400

    uncommented code

--- components/Form/AI/AI.jsx
+++ components/Form/AI/AI.jsx
@@ -2,11 +2,21 @@ import { useState } from "react";
 import Form from "../Form";
 
 const AI = () => {
-	const [responses, setResponses] = useState([]);
-	console.log(responses);
+	const [results, setResults] = useState([]);
+
+	const setResultsHandler = (data) => {
+		let result = { prompt: `${data.prompt}`, result: `${data.result}` };
+		// let newResults = [result, ...results];
+		setResults((currentResults) => {
+			return [result, ...currentResults];
+		});
+		console.log("result", result);
+		console.log("Results List", results);
+	};
+
 	return (
 		<div>
-			<Form directions={directions} setDirections={setDirections} />
+			<Form setResults={setResultsHandler} />
 		</div>
 	);
 };
