a734449c37771b4024805c0f874eea934ee799ab
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-21 14:53:42 -0400

    moved icon to the right of screen

--- components/Form/ThemeToggler/ThemeToggle.js
+++ components/Form/ThemeToggler/ThemeToggle.js
@@ -1,16 +1,23 @@
-import { useColorMode, useColorModeValue, IconButton } from "@chakra-ui/react";
+import {
+	useColorMode,
+	useColorModeValue,
+	IconButton,
+	Box,
+} from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const ThemeToggle = () => {
 	const { toggleColorMode } = useColorMode();
 
 	return (
-		<IconButton
-			aria-label="Toggle Theme"
-			icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-			onClick={toggleColorMode}
-			variant="ghost"
-		></IconButton>
+		<Box flex={1} align="right">
+			<IconButton
+				aria-label="Toggle Theme"
+				icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+				onClick={toggleColorMode}
+				variant="ghost"
+			></IconButton>
+		</Box>
 	);
 };
 
