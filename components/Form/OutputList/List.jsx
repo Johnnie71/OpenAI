14d5a020112a987d71a9e52a5177d013c639d4f9
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-22 02:52:27 -0400

    displaying results

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -16,28 +16,31 @@ const List = ({ results }) => {
 		<Flex maxW="full" align="center" justify="center">
 			<Grid w="50%" autoColumns gap={5}>
 				{results.length > 0
-					? results.map((result, idx) => (
-							<Box
-								boxShadow="xl"
-								borderRadius="10px"
-								width="100%"
-								bg={colorTheme}
-								key={idx}
-							>
-								<Flex color="black">
-									<GridItem p={5}>Prompt:</GridItem>
-									<GridItem ml={20} p={5}>
-										{result.prompt}
-									</GridItem>
-								</Flex>
-								<Flex color="black">
-									<GridItem p={5}>Response:</GridItem>
-									<GridItem ml={16} p={5}>
-										{result.response}
-									</GridItem>
-								</Flex>
-							</Box>
-					  ))
+					? results.map((item, idx) => {
+							const { prompt, result } = item;
+							return (
+								<Box
+									boxShadow="xl"
+									borderRadius="10px"
+									width="100%"
+									bg={colorTheme}
+									key={idx}
+								>
+									<Flex color="black">
+										<GridItem p={5}>Prompt:</GridItem>
+										<GridItem ml={20} p={5}>
+											{prompt}
+										</GridItem>
+									</Flex>
+									<Flex color="black">
+										<GridItem p={5}>Response:</GridItem>
+										<GridItem ml={16} p={5}>
+											{result}
+										</GridItem>
+									</Flex>
+								</Box>
+							);
+					  })
 					: null}
 			</Grid>
 		</Flex>
