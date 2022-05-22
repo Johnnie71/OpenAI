bf0ba227467d5de0320395185d6006005bd08480
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-22 02:03:06 -0400

    testing results

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -3,19 +3,19 @@ import { Box, Grid, GridItem, Flex, useColorModeValue } from "@chakra-ui/react";
 const List = ({ results }) => {
 	const colorTheme = useColorModeValue("peachpuff", "#DEB6AB");
 
-	const tests = [
-		{
-			prompt:
-				"This is what you came for.............. ......... ....... ......... ........ ......",
-			response: "Rihanna",
-		},
-		{ prompt: "Imma hit you with a dose!", response: "Ciara" },
-		{ prompt: "Turn up the music!", response: "Chris Brown" },
-	];
+	// const tests = [
+	// 	{
+	// 		prompt:
+	// 			"This is what you came for.............. ......... ....... ......... ........ ......",
+	// 		response: "Rihanna",
+	// 	},
+	// 	{ prompt: "Imma hit you with a dose!", response: "Ciara" },
+	// 	{ prompt: "Turn up the music!", response: "Chris Brown" },
+	// ];
 	return (
 		<Flex maxW="full" align="center" justify="center">
 			<Grid w="50%" autoColumns gap={5}>
-				{tests.map((result, idx) => (
+				{results.map((result, idx) => (
 					<Box
 						boxShadow="xl"
 						borderRadius="10px"
