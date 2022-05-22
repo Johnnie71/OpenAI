d45bad9c6f003b921a7ace82f6181724ea59e436
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-22 02:52:01 -0400

    displaying results

--- components/Form/AI/AI.jsx
+++ components/Form/AI/AI.jsx
@@ -5,31 +5,31 @@ import { Container, Flex, Heading } from "@chakra-ui/react";
 import ThemeToggle from "../ThemeToggler/ThemeToggle";
 
 const AI = () => {
-	let savedResults = useRef();
+	// let savedResults = useRef();
+	const [results, setResults] = useState([]);
+	// const [results, setResults] = useState(() => {
+	// if there are results stored in localStorage
+	// if (savedResults) {
+	// return the parsed JSON object back to a javascript object
+	// 	return savedResults;
+	// } else {
+	// return an empty array
+	// 		return [];
+	// 	}
+	// });
 
-	const [results, setResults] = useState(() => {
-		// if there are results stored in localStorage
-		if (savedResults) {
-			console.log(savedResults);
-			// return the parsed JSON object back to a javascript object
-			return savedResults;
-		} else {
-			// return an empty array
-			return [];
-		}
-	});
+	// useEffect(() => {
+	// 	let resultsInStorage = localStorage.getItem("results");
 
-	useEffect(() => {
-		let resultsInStorage = localStorage.getItem("results");
-
-		if (resultsInStorage) {
-			savedResults.current = resultsInStorage;
-		} else {
-			localStorage.setItem("results", JSON.stringify(results));
-		}
-	}, [results]);
+	// 	if (resultsInStorage) {
+	// 		savedResults.current = resultsInStorage;
+	// 	} else {
+	// 		localStorage.setItem("results", JSON.stringify(results));
+	// 	}
+	// }, [results]);
 
 	const setResultsHandler = (data) => {
+		console.log("DATA: ", data);
 		let result = { prompt: data.prompt, result: data.result };
 		setResults((currentResults) => {
 			return [result, ...currentResults];
