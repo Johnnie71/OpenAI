b8bf5339c885f610a9a9d12ef106d9b67b91b780
johnnie71 <johnnie.agonz71@gmail.com>
2022-05-24 19:56:11 -0400

    mobile view

--- components/Form/OutputList/List.jsx
+++ components/Form/OutputList/List.jsx
@@ -1,4 +1,11 @@
-import { Box, Grid, GridItem, Flex, useColorModeValue } from "@chakra-ui/react";
+import {
+	Box,
+	Grid,
+	GridItem,
+	Flex,
+	useColorModeValue,
+	useMediaQuery,
+} from "@chakra-ui/react";
 
 const List = ({ results }) => {
 	const colorTheme = useColorModeValue("peachpuff", "#202023");
@@ -7,6 +14,7 @@ const List = ({ results }) => {
 		"6px 5px 10px grey",
 		"6px 5px 10px white"
 	);
+	const [isMobile] = useMediaQuery("(max-width: 768px)");
 
 	return (
 		<Flex mb={10} maxW="full" align="center" justify="center">
@@ -26,18 +34,34 @@ const List = ({ results }) => {
 									bg={colorTheme}
 									key={idx}
 								>
-									<Flex color={fontColor}>
-										<GridItem p={5}>Prompt:</GridItem>
-										<GridItem ml={20} p={5}>
-											{prompt}
-										</GridItem>
-									</Flex>
-									<Flex color={fontColor}>
-										<GridItem p={5}>Response:</GridItem>
-										<GridItem ml={16} p={5}>
-											{newResult}
-										</GridItem>
-									</Flex>
+									{isMobile ? (
+										<>
+											<Box textDecorationLine="underline">Prompt:</Box>
+											<Box p={2} as="p">
+												{prompt}
+											</Box>
+
+											<Box textDecorationLine="underline">Response:</Box>
+											<Box as="p" p={2}>
+												{newResult}
+											</Box>
+										</>
+									) : (
+										<>
+											<Flex color={fontColor}>
+												<GridItem p={5}>Prompt:</GridItem>
+												<GridItem ml={20} p={5}>
+													{prompt}
+												</GridItem>
+											</Flex>
+											<Flex color={fontColor}>
+												<GridItem p={5}>Response:</GridItem>
+												<GridItem ml={16} p={5}>
+													{newResult}
+												</GridItem>
+											</Flex>
+										</>
+									)}
 								</Box>
 							);
 					  })
